87f1d9887e1d3d81119e95ae9ab88e477cd0cd2a
mpost <mpost@192.168.0.46>
2019-10-27 22:46:06 -0700

    I'mFairlyShockedThatICompletedThis.java

--- src/Equestria.java
+++ src/Equestria.java
@@ -8,6 +8,20 @@ public class Equestria {
 		int manehattanTwo = 9;
 		double total = distance(baltimareOne, baltimareTwo, manehattanOne, manehattanTwo);
 		System.out.println("The distance from Baltimare to Manehattan is " + total + " sparkle fairy pony units");
+
+		int losPegasusOne = 6;
+		int losPegasusTwo = 20;
+		int neighagraFallsOne = 24;
+		int neighagraFallsTwo = 7;
+		double newtotal = distance(losPegasusOne, losPegasusTwo, neighagraFallsOne, neighagraFallsTwo);
+		System.out.println("The distance from Los Pegasus to Neighagra Falls is " + newtotal + " sparkle fairy pony units");
+		
+		int badlandsOne = 28;
+		int badlandsTwo = 22;
+		int ponyvilleOne = 16;
+		int ponyvilleTwo = 13;
+		double newNewTotal = distance(badlandsOne, badlandsTwo, ponyvilleOne, ponyvilleTwo);
+		System.out.println("The distance from the Badlands to Ponyville is " + newNewTotal + " sparkle fairy pony units");
 	}
 	
 	public static double distance(double exOne, double whyOne, double exTwo, double whyTwo) {
